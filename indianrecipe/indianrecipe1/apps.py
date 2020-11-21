00944809aa8789b9f47a6d93c97ac4712e73052b
Satish84461 <satish84461@gmail.com>
2020-11-21 22:37:21 +0530

    Removed

--- indianrecipe/indianrecipe1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Indianrecipe1Config(AppConfig):
-    name = 'indianrecipe1'
--- indianrecipe/indianrecipe1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = {
-    path('/home',views.index,name='index'),
-    path('/menu',views.menu,name='menu'),
-    path('/aboutus',views.aboutus,name='aboutus'),
-    path('/contact',views.contact,name='contact'),
-}
